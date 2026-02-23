ba7e1fda670745e6b7c0bd952dabdac45715c168
cauepda <cauepda@al.insper.edu.br>
2026-02-23 18:07:13 -0300

    roteiro 1

--- main.cpp
+++ main.cpp
@@ -1,23 +1,39 @@
 #include <sstream>
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
 // int argc   : Argument Count
 // char *argv : Argument Vector
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        throw std::invalid_argument("Nenhuma expressao fornecida.");
+    }
+
     std::istringstream ss(argv[1]);
 
     int resultado_atual;
-    char op;
-    int proximo_n;
-
     if (!(ss >> resultado_atual)) {
         throw std::invalid_argument("Expressao nao comeca com um numero.");
         return 1;
     }
-    
-    while (ss >> op >> proximo_n) {
+
+
+    char op;
+    int proximo_n;
+    bool leu_algum_operador = false;
+
+    while (true) {
+        if (ss >> op) {
+            break;
+        }
+        leu_algum_operador = true;
+
+        if (!(ss >> proximo_n)) {
+            throw std::invalid_argument("Faltando numero apos operador.");
+        }
+
         if (op == '+') {
             resultado_atual += proximo_n;
         }
@@ -25,11 +41,16 @@ int main(int argc, char *argv[]) {
             resultado_atual -= proximo_n;
         }
         else {
-            std::cerr << "Erro de sintaxe: Operador '" << op << "' não reconhecido.\n";
-            return 1;
+            throw std::invalid_argument(std::string("Operador não reconhecido: ") + op);
         }
     }
 
-    std::cout << resultado_atual;
+    if (!ss.eof() && !ss.fail()) {
+         throw std::invalid_argument("Expressao mal formatada no final.");
+    }
+    if (!leu_algum_operador) {
+        throw std::invalid_argument("Expressao incompleta (sem operador).");
+    }
+    std::cout << resultado_atual << "\n";
     return 0;
 }
